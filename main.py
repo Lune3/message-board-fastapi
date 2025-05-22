c42f29a27302b713a3e20365248b49d2a25aa67a
Lune3 <113820374+Lune3@users.noreply.github.com>
2025-05-22 15:49:21 +0000

    Adding post route

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, List
+from fastapi import FastAPI, Request, Response
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 from datetime import date
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+class message(BaseModel):
+    name: str
+    text: str
+    added: date
+
 today = date.today()
 
-message = [{'text':"Hi There!",'user':"John",'added':today},{'text':"Hello World",'user':"Jane",'added':today}]
+messages: List[message] = [
+    message(text="Hi There!", name="John", added=today),
+    message(text="Hello World", name="Jane", added=today)
+]
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request,name="index.html",context={'messages':messages}
+    )
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/new")
+def formPage(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="form.html"
+    )
+
+@app.post("/new")
+def formPagePost(request: Request, response: Response,message: message):
+    messages.append(message)
+    
+
